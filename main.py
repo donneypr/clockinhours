9e9d9272799b6ef29587fc1a2c0cba6bc144a245
donneypr <donatoprabahar@gmail.com>
2025-05-04 06:52:45 -0400

    added login and bypass

--- main.py
+++ main.py
@@ -86,6 +86,85 @@ def log_into_w2w(driver, website):
             print("-----------------------")
 
 
+def login_and_bypass_verification(driver, website_url, myusername, mypassword, mybypasscode):
+     try:
+         driver.get(website_url)
+         wait = WebDriverWait(driver, 10)
+         username_input = wait.until(
+             EC.presence_of_element_located((By.ID, "mli"))
+         )
+         username_input.clear()
+         username_input.send_keys(myusername)
+ 
+         # Enter password
+         password_input = driver.find_element(By.NAME, "password")
+         password_input.clear()
+         password_input.send_keys(mypassword)
+ 
+         # Click login button
+         log_in = driver.find_element(By.NAME, "dologin")
+         log_in.click()
+ 
+         time.sleep(5)
+         print("Logged in successfully. Waiting for user action.")
+ 
+         # Wait for verification code element
+         wait = WebDriverWait(driver, 10)
+         verification_code_element = wait.until(
+             EC.presence_of_element_located((By.CLASS_NAME, "verification-code"))
+         )
+         print("Verification code content:", verification_code_element.text)
+ 
+         time.sleep(5)
+ 
+         # Click 'Other Options'
+         other_options_button = wait.until(
+             EC.element_to_be_clickable((By.CLASS_NAME, "other-options-link"))
+         )
+         other_options_button.click()
+ 
+         time.sleep(1)
+ 
+         try:
+             bypass_code_link = WebDriverWait(driver, 20).until(
+                 EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="test-id-bypass"]'))
+             )
+             bypass_code_link.click()
+             print("Successfully clicked the bypass code link.")
+         except Exception as e:
+             print(f"An error occurred: {e}")
+ 
+         # Enter bypass code
+         bypass_code_input = wait.until(
+             EC.visibility_of_element_located((By.ID, "passcode-input"))
+         )
+         bypass_code_input.send_keys(mybypasscode)
+         print("Bypass code inputted.")
+ 
+         try:
+             verify_button = WebDriverWait(driver, 20).until(
+                 EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="verify-button"]'))
+             )
+             verify_button.click()
+             print("Successfully clicked the verify button.")
+         except Exception as e:
+             print(f"An error occurred while clicking the verify button: {e}")
+ 
+         try:
+             trust_browser_button = WebDriverWait(driver, 20).until(
+                 EC.element_to_be_clickable((By.ID, "trust-browser-button"))
+             )
+             trust_browser_button.click()
+             print("Successfully clicked the 'Trust Browser' button.")
+         except Exception as e:
+             print(f"An error occurred while clicking the button: {e}")
+ 
+         time.sleep(10)
+ 
+     except Exception as e:
+         print(f"An error occurred during the login and verification process: {e}")
+
+
 open_broswer(driver, w2work)
 time.sleep(1)
 log_into_w2w(driver, w2work)
