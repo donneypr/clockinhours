1979fc4ae03bc869a130c11158c454b7a2231bb4
donneypr <donatoprabahar@gmail.com>
2025-05-04 05:36:05 -0400

    not headless for testing

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ myusername = os.getenv("USERNAME")
 mypassword = os.getenv("PASSWORD")
 mybypasscode = os.getenv("BYPASSCODE")
 
-w2Work = "https://www6.whentowork.com/cgi-bin/w2wF3.dll/empschedule?SID=320330770411C&lmi="
+w2work = "https://www6.whentowork.com/cgi-bin/w2wF3.dll/empschedule?SID=320330770411C&lmi="
 hrSelfServe = "https://hrselfserve.info.yorku.ca/"
 
+''''''
 #headles setting
 chrome_options = Options()
 chrome_options.add_argument("--disable-popup-blocking") 
 chrome_options.add_argument("--disable-notifications")  
-chrome_options.add_argument("--headless")  
+#chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--disable-gpu")  
 
 #chromedriver
@@ -39,102 +40,5 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 def open_broswer(driver, website):
     driver.get(website)
 
-def login_and_bypass_verification(driver, website_url, myusername, mypassword, mybypasscode):
-    try:
-        # Navigate to the website
-        driver.get(website_url)
-        # Enter username
-        wait = WebDriverWait(driver, 10)
-        username_input = wait.until(
-            EC.presence_of_element_located((By.ID, "mli"))
-        )
-        username_input.clear()
-        username_input.send_keys(myusername)
-
-        # Enter password
-        password_input = driver.find_element(By.NAME, "password")
-        password_input.clear()
-        password_input.send_keys(mypassword)
-
-        # Click login button
-        log_in = driver.find_element(By.NAME, "dologin")
-        log_in.click()
-
-        time.sleep(5)
-        print("Logged in successfully. Waiting for user action.")
-
-        # Wait for verification code element
-        wait = WebDriverWait(driver, 10)
-        verification_code_element = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "verification-code"))
-        )
-        print("Verification code content:", verification_code_element.text)
-
-        time.sleep(5)
-
-        # Click 'Other Options'
-        other_options_button = wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "other-options-link"))
-        )
-        other_options_button.click()
-
-        time.sleep(1)
-
-        # Click 'Bypass Code' link
-        try:
-            bypass_code_link = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="test-id-bypass"]'))
-            )
-            bypass_code_link.click()
-            print("Successfully clicked the bypass code link.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-        # Enter bypass code
-        bypass_code_input = wait.until(
-            EC.visibility_of_element_located((By.ID, "passcode-input"))
-        )
-        bypass_code_input.send_keys(mybypasscode)
-        print("Bypass code inputted.")
-
-        # Click verify button
-        try:
-            verify_button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="verify-button"]'))
-            )
-            verify_button.click()
-            print("Successfully clicked the verify button.")
-        except Exception as e:
-            print(f"An error occurred while clicking the verify button: {e}")
-
-        # Click 'Trust Browser' button
-        try:
-            trust_browser_button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.ID, "trust-browser-button"))
-            )
-            trust_browser_button.click()
-            print("Successfully clicked the 'Trust Browser' button.")
-        except Exception as e:
-            print(f"An error occurred while clicking the button: {e}")
-
-        time.sleep(10)
-
-    except Exception as e:
-        print(f"An error occurred during the login and verification process: {e}")
-
-
-def clockInHours():
-    absence_time = wait.until(
-        EC.element_to_be_clickable((By.ID, "PTNUI_LAND_REC_GROUPLET_LBL$1"))
-    )
-    absence_time.click()
-
-    enter_time = wait.until(
-        EC.elememt_to_be_clickable((By.ID, "PTNUI_LAND_REC_GROUPLET_LBL$1"))
-    )
-    enter_time.click()
-
-    ##have logic to ensure that it is on the page of the right week and clock in the hours for the right day
-
-    
+open_broswer(driver, w2work)
 
